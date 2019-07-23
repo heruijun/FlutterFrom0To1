7ab8b05ea298fd0a46ba0626dc05d5f7018f6783
何瑞君 <heruijun@heruijundeMacBook-Pro.local>
2019-07-23 14:01:27 +0800

    360旋转展物

--- chapter4/gesture_detector_360/build/app/intermediates/flutter/release/android-arm64/frontend_server.d
+++
@@ -1,1 +0,0 @@
-frontend_server.d: /Users/heruijun/Documents/flutter/bin/cache/artifacts/engine/darwin-x64/frontend_server.dart.snapshot
--- chapter4/gesture_detector_360/build/app/intermediates/flutter/release/android-arm64/gen_snapshot.d
+++
@@ -1,1 +0,0 @@
-gen_snapshot.d: /Users/heruijun/Documents/flutter/bin/cache/artifacts/engine/android-arm64-release/darwin-x64/gen_snapshot
